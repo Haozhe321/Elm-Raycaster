b8ca522758eae035d242d739acb1086e11c894d2
Haozhe <hzthegreat@gmail.com>
2019-04-05 17:34:17 +0800

    Initial commit v3

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,12 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html
 
-
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
-
-
-
----- PROGRAM ----
-
-
-main : Program () Model Msg
+main : Program Never Types.Model Types.Msg
 main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
+    Html.program
+        { init = ( model, Cmd msg )
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
+        , view = view
         }
