45797e9f886bc2557a178e19cd2819362f3118f2
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2019-04-05 20:40:57 +0800

    Add interactive circle drawing on Mouse Position

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,78 @@
 module Main exposing (main)
 
-import Html
+{-| Entry point for the app
 
-main : Program Never Types.Model Types.Msg
+@docs main
+-}
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+import Html
+import Platform.Cmd as Cmd
+import Mouse
+-- Build World State
+
+type alias Model =
+     {
+      mouseInput : Maybe Mouse.Position,
+      mouseHistory : List Mouse.Position
+     }
+
+type Msg =
+    MouseXY Mouse.Position
+
+empty_model : (Model, Cmd msg)
+empty_model = ({
+                mouseInput = Nothing,
+                mouseHistory = []
+                }, Cmd.none)
+
+make_circle position =
+    let
+        x_str = toString position.x
+        y_str = toString position.y
+    in
+        circle [cx x_str, cy y_str, r "20", fill "blue"] []
+
+view : Model -> Html.Html msg
+view model =
+    case model.mouseInput of
+        Just position ->
+            let
+                x_str = toString position.x
+                y_str = toString position.y
+            in svg
+                [width "800", height "800"]
+                (
+                [
+                    circle [cx "600", cy "620", r "200", fill "green"] [],
+                    make_circle position
+                ] ++
+                List.map (\position -> make_circle position) model.mouseHistory
+                )
+        _ -> Html.text"Loading."
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MouseXY mouse ->
+            ( { model | mouseInput = Just mouse
+                        , mouseHistory = List.take 10 (mouse :: model.mouseHistory)}
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Mouse.moves MouseXY
+
+{-| Start the program running.
+-}
+main : Program Never Model Msg
 main =
     Html.program
-        { init = ( model, Cmd msg )
+        { init = empty_model
         , update = update
         , subscriptions = subscriptions
         , view = view
